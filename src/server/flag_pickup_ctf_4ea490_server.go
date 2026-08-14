d475c7b414be7224fabc39a26471379c05131bbd
Hyojeon Yoon <hjyoon@kakao.com>
2026-08-14 20:07:01 +0900

    restore: bind CTF flag pickup to native fields

---
+++ src/server/flag_pickup_ctf_4ea490_server.go
@@ -0,0 +1,246 @@
+package server
+
+import (
+	"unsafe"
+
+	"github.com/opennox/libs/object"
+	"github.com/opennox/libs/strman"
+	"github.com/opennox/libs/types"
+)
+
+// FlagUpdateData4EA490 is the pointer-free prefix used by team flags. Its
+// three original ABI32 words retain the same offsets on every Go target.
+type FlagUpdateData4EA490 struct {
+	Home  types.Pointf
+	State uint32
+}
+
+var (
+	_ = [1]struct{}{}[12-unsafe.Sizeof(FlagUpdateData4EA490{})]
+	_ = [1]struct{}{}[8-unsafe.Offsetof(FlagUpdateData4EA490{}.State)]
+)
+
+// FlagPickupCTFRuntime4EA490 supplies effects still owned by the legacy/root
+// runtime. Native Object, update-data, Player, and Team fields are handled by
+// the server adapter without pointer-to-int conversions.
+type FlagPickupCTFRuntime4EA490 struct {
+	FlagIndex       func(*Object) uint32
+	GameData        func(uint32) uint16
+	MoveHome        func(*Object, *FlagUpdateData4EA490)
+	InformReturn    func(uint32)
+	InformFlag      func(uint32, uint32, uint32)
+	FlagStatus      func(uint8, uint8, uint8, uint16) int32
+	ObserverMode    func() uint32
+	ObserverUpdate  func(*Player, *Player)
+	DetachInventory func(*Object, *Object)
+	CreateAt        func(*Object, *Object, types.Pointf)
+	SetGameFlags    func(uint32)
+	FlagWinner      func(*Team, uint32)
+	TeamEligible    func(*Team) int32
+	ForceDrop       func(*Object, *Object)
+	FinalizeDelete  func(*Object)
+	InventoryPut    func(*Object, *Object, int32)
+	ReportObject    func(uint32, *Object)
+	BuffPurge       FlagPickupBuffPurgeRuntime4EA7A0
+}
+
+type flagPickupCTFNativeDeps4EA490 struct {
+	flagIndex       func(*Object) uint32
+	gameData        func(uint32) uint16
+	moveHome        func(*Object, *FlagUpdateData4EA490)
+	informReturn    func(uint32)
+	informFlag      func(uint32, uint32, uint32)
+	flagStatus      func(uint8, uint8, uint8, uint16) int32
+	reportLesson    func(*Object)
+	teamByID        func(uint8) *Team
+	changeTeamScore func(*Team, int32)
+	observerMode    func() uint32
+	observerUpdate  func(*Player, *Player)
+	detachInventory func(*Object, *Object)
+	createAt        func(*Object, *Object, types.Pointf)
+	raise           func(*Object, float32)
+	markMinimap     func(*Object, uint32)
+	firstTeam       func() *Team
+	nextTeam        func(*Team) *Team
+	setGameFlags    func(uint32)
+	flagWinner      func(*Team, uint32)
+	teamEligible    func(*Team) int32
+	forceDrop       func(*Object, *Object)
+	finalizeDelete  func(*Object)
+	inventoryPut    func(*Object, *Object, int32)
+	reportObject    func(uint32, *Object)
+	unmarkMinimap   func(*Object, uint32)
+	purgeBuffs      func(*Object)
+	priorityMessage func(*Object, string, uint32)
+}
+
+func flagPickupCTFNative4EA490(
+	source, target *Object,
+	collision *types.Pointf,
+	deps flagPickupCTFNativeDeps4EA490,
+) {
+	flagPickupCTF4EA490(source, target, collision,
+		flagPickupCTFHooks4EA490[*Object, unsafe.Pointer, *Team, *Player]{
+			loadUpdate: func(obj *Object) unsafe.Pointer {
+				return obj.UpdateData
+			},
+			flagIndex: deps.flagIndex,
+			loadTeamID: func(obj *Object) uint8 {
+				return uint8(obj.TeamVal.ID)
+			},
+			teamsSame: func(target, source *Object) int32 {
+				if target.TeamVal.SameAs(&source.TeamVal) {
+					return 1
+				}
+				return 0
+			},
+			loadPosX: func(obj *Object) float32 {
+				return obj.PosVec.X
+			},
+			loadPosY: func(obj *Object) float32 {
+				return obj.PosVec.Y
+			},
+			loadHomeX: func(update unsafe.Pointer) float32 {
+				return (*FlagUpdateData4EA490)(update).Home.X
+			},
+			loadHomeY: func(update unsafe.Pointer) float32 {
+				return (*FlagUpdateData4EA490)(update).Home.Y
+			},
+			moveHome: func(obj *Object, update unsafe.Pointer) {
+				deps.moveHome(obj, (*FlagUpdateData4EA490)(update))
+			},
+			loadNetCode: func(obj *Object) uint32 {
+				return obj.NetCode
+			},
+			informReturn: deps.informReturn,
+			informFlag:   deps.informFlag,
+			storeFlagState: func(update unsafe.Pointer, state uint32) {
+				(*FlagUpdateData4EA490)(update).State = state
+			},
+			flagStatus: deps.flagStatus,
+			firstInventory: func(obj *Object) *Object {
+				return obj.InvFirstItem
+			},
+			nextInventory: func(obj *Object) *Object {
+				return obj.InvNextItem
+			},
+			loadClass: func(obj *Object) uint32 {
+				return uint32(obj.ObjClass)
+			},
+			gameData: deps.gameData,
+			changeScore: func(obj *Object, delta int32) {
+				if uint8(obj.ObjClass)&uint8(object.ClassPlayer) != 0 {
+					update := (*PlayerUpdateData)(obj.UpdateData)
+					update.Player.Lessons += delta
+				}
+			},
+			reportLesson: deps.reportLesson,
+			hasTeam: func(obj *Object) int32 {
+				if obj.TeamVal.Has() {
+					return 1
+				}
+				return 0
+			},
+			teamByID: deps.teamByID,
+			loadTeamScore: func(team *Team) int32 {
+				return int32(team.Lessons)
+			},
+			changeTeamScore: deps.changeTeamScore,
+			observerMode:    deps.observerMode,
+			playerFromUpdate: func(update unsafe.Pointer) *Player {
+				return (*PlayerUpdateData)(update).Player
+			},
+			observerUpdate:  deps.observerUpdate,
+			detachInventory: deps.detachInventory,
+			createAt: func(obj, owner *Object, x, y float32) {
+				deps.createAt(obj, owner, types.Pointf{X: x, Y: y})
+			},
+			raise:        deps.raise,
+			markMinimap:  deps.markMinimap,
+			firstTeam:    deps.firstTeam,
+			nextTeam:     deps.nextTeam,
+			setGameFlags: deps.setGameFlags,
+			flagWinner:   deps.flagWinner,
+			inventoryHolder: func(obj *Object) *Object {
+				return obj.InvHolder
+			},
+			teamEligible:   deps.teamEligible,
+			forceDrop:      deps.forceDrop,
+			finalizeDelete: deps.finalizeDelete,
+			inventoryPut:   deps.inventoryPut,
+			markPlayerPickup: func(update unsafe.Pointer, flags uint32) {
+				(*PlayerUpdateData)(update).Player.WeaponEquip |= flags
+			},
+			reportObject:    deps.reportObject,
+			unmarkMinimap:   deps.unmarkMinimap,
+			purgeBuffs:      deps.purgeBuffs,
+			priorityMessage: deps.priorityMessage,
+		},
+	)
+}
+
+func flagPickupCTFMarkMinimapAll4EA490(s *Server, obj *Object, flags uint32) {
+	for player := s.Players.First(); player != nil; player = s.Players.Next(player) {
+		s.Players.Nox_xxx_netMarkMinimapObject_417190(player.PlayerIndex(), obj, flags)
+	}
+}
+
+func flagPickupCTFServerDeps4EA490(
+	s *Server,
+	runtime FlagPickupCTFRuntime4EA490,
+) flagPickupCTFNativeDeps4EA490 {
+	return flagPickupCTFNativeDeps4EA490{
+		flagIndex:    runtime.FlagIndex,
+		gameData:     runtime.GameData,
+		moveHome:     runtime.MoveHome,
+		informReturn: runtime.InformReturn,
+		informFlag:   runtime.InformFlag,
+		flagStatus:   runtime.FlagStatus,
+		reportLesson: s.Nox_xxx_netReportLesson_4D8EF0,
+		teamByID: func(id uint8) *Team {
+			return s.Teams.ByID(TeamID(id))
+		},
+		changeTeamScore: func(team *Team, score int32) {
+			s.TeamChangeLessons(team, int(score))
+		},
+		observerMode:    runtime.ObserverMode,
+		observerUpdate:  runtime.ObserverUpdate,
+		detachInventory: runtime.DetachInventory,
+		createAt:        runtime.CreateAt,
+		raise: func(obj *Object, z float32) {
+			obj.Raise(z)
+		},
+		markMinimap: func(obj *Object, flags uint32) {
+			flagPickupCTFMarkMinimapAll4EA490(s, obj, flags)
+		},
+		firstTeam:      s.Teams.First,
+		nextTeam:       s.Teams.Next,
+		setGameFlags:   runtime.SetGameFlags,
+		flagWinner:     runtime.FlagWinner,
+		teamEligible:   runtime.TeamEligible,
+		forceDrop:      runtime.ForceDrop,
+		finalizeDelete: runtime.FinalizeDelete,
+		inventoryPut:   runtime.InventoryPut,
+		reportObject:   runtime.ReportObject,
+		unmarkMinimap: func(obj *Object, flags uint32) {
+			s.Players.Nox_xxx_netUnmarkMinimapSpec_417470(obj, flags)
+		},
+		purgeBuffs: func(obj *Object) {
+			s.FlagPickupBuffPurge4EA7A0(obj, runtime.BuffPurge)
+		},
+		priorityMessage: func(obj *Object, message string, arg uint32) {
+			s.NetPriMsgToPlayer(obj, strman.ID(message), byte(arg))
+		},
+	}
+}
+
+// FlagPickupCTF4EA490 runs the native-width CTF flag handler. The legacy C
+// export is installed only after the sibling FlagBall handler and router are
+// restored, so no 64-bit callback can fall back into the old int-address body.
+func (s *Server) FlagPickupCTF4EA490(
+	source, target *Object,
+	collision *types.Pointf,
+	runtime FlagPickupCTFRuntime4EA490,
+) {
+	flagPickupCTFNative4EA490(source, target, collision, flagPickupCTFServerDeps4EA490(s, runtime))
+}
---
+++ src/server/flag_pickup_ctf_4ea490_server_test.go
@@ -0,0 +1,307 @@
+package server
+
+import (
+	"testing"
+	"unsafe"
+
+	"github.com/opennox/libs/object"
+	"github.com/opennox/libs/types"
+)
+
+func defaultFlagPickupCTFNativeDeps4EA490() flagPickupCTFNativeDeps4EA490 {
+	return flagPickupCTFNativeDeps4EA490{
+		flagIndex:       func(*Object) uint32 { return 0 },
+		gameData:        func(uint32) uint16 { return 0 },
+		moveHome:        func(*Object, *FlagUpdateData4EA490) {},
+		informReturn:    func(uint32) {},
+		informFlag:      func(uint32, uint32, uint32) {},
+		flagStatus:      func(uint8, uint8, uint8, uint16) int32 { return 0 },
+		reportLesson:    func(*Object) {},
+		teamByID:        func(uint8) *Team { return nil },
+		changeTeamScore: func(*Team, int32) {},
+		observerMode:    func() uint32 { return 0 },
+		observerUpdate:  func(*Player, *Player) {},
+		detachInventory: func(*Object, *Object) {},
+		createAt:        func(*Object, *Object, types.Pointf) {},
+		raise:           func(*Object, float32) {},
+		markMinimap:     func(*Object, uint32) {},
+		firstTeam:       func() *Team { return nil },
+		nextTeam:        func(*Team) *Team { return nil },
+		setGameFlags:    func(uint32) {},
+		flagWinner:      func(*Team, uint32) {},
+		teamEligible:    func(*Team) int32 { return 0 },
+		forceDrop:       func(*Object, *Object) {},
+		finalizeDelete:  func(*Object) {},
+		inventoryPut:    func(*Object, *Object, int32) {},
+		reportObject:    func(uint32, *Object) {},
+		unmarkMinimap:   func(*Object, uint32) {},
+		purgeBuffs:      func(*Object) {},
+		priorityMessage: func(*Object, string, uint32) {},
+	}
+}
+
+func TestFlagPickupCTF4EA490NativeLayout(t *testing.T) {
+	wantObjectUpdate := uintptr(748)
+	wantObjectTeam := uintptr(48)
+	wantObjectPos := uintptr(56)
+	wantObjectNet := uintptr(36)
+	wantObjectHolder := uintptr(492)
+	wantObjectNext := uintptr(496)
+	wantObjectFirst := uintptr(504)
+	wantPlayerUpdatePlayer := uintptr(276)
+	wantPlayerLessons := uintptr(2136)
+	wantTeamLessons := uintptr(52)
+	wantTeamID := uintptr(57)
+	if unsafe.Sizeof(uintptr(0)) == 8 {
+		wantObjectUpdate = 872
+		wantObjectTeam = 52
+		wantObjectPos = 60
+		wantObjectNet = 40
+		wantObjectHolder = 520
+		wantObjectNext = 528
+		wantObjectFirst = 544
+		wantPlayerUpdatePlayer = 320
+		wantPlayerLessons = 2140
+		wantTeamLessons = 56
+		wantTeamID = 65
+	}
+	checks := []struct {
+		name string
+		got  uintptr
+		want uintptr
+	}{
+		{"FlagUpdateData size", unsafe.Sizeof(FlagUpdateData4EA490{}), 12},
+		{"FlagUpdateData.Home", unsafe.Offsetof(FlagUpdateData4EA490{}.Home), 0},
+		{"FlagUpdateData.State", unsafe.Offsetof(FlagUpdateData4EA490{}.State), 8},
+		{"Object.UpdateData", unsafe.Offsetof(Object{}.UpdateData), wantObjectUpdate},
+		{"Object.TeamVal", unsafe.Offsetof(Object{}.TeamVal), wantObjectTeam},
+		{"Object.PosVec", unsafe.Offsetof(Object{}.PosVec), wantObjectPos},
+		{"Object.NetCode", unsafe.Offsetof(Object{}.NetCode), wantObjectNet},
+		{"Object.InvHolder", unsafe.Offsetof(Object{}.InvHolder), wantObjectHolder},
+		{"Object.InvNextItem", unsafe.Offsetof(Object{}.InvNextItem), wantObjectNext},
+		{"Object.InvFirstItem", unsafe.Offsetof(Object{}.InvFirstItem), wantObjectFirst},
+		{"PlayerUpdateData.Player", unsafe.Offsetof(PlayerUpdateData{}.Player), wantPlayerUpdatePlayer},
+		{"Player.WeaponEquip", unsafe.Offsetof(Player{}.WeaponEquip), 4},
+		{"Player.Lessons", unsafe.Offsetof(Player{}.Lessons), wantPlayerLessons},
+		{"Team.Lessons", unsafe.Offsetof(Team{}.Lessons), wantTeamLessons},
+		{"Team.IDVal", unsafe.Offsetof(Team{}.IDVal), wantTeamID},
+	}
+	for _, check := range checks {
+		if check.got != check.want {
+			t.Errorf("%s = %d, want %d", check.name, check.got, check.want)
+		}
+	}
+}
+
+func TestFlagPickupCTFNative4EA490ReturnHomeUsesTypedUpdate(t *testing.T) {
+	update := &FlagUpdateData4EA490{Home: types.Pointf{X: 1, Y: 2}, State: 99}
+	targetUpdate := &PlayerUpdateData{}
+	source := &Object{
+		TeamVal:    ObjectTeam{ID: 5},
+		PosVec:     types.Pointf{X: 7, Y: 2},
+		UpdateData: unsafe.Pointer(update),
+	}
+	target := &Object{
+		NetCode:    0x12345678,
+		TeamVal:    ObjectTeam{ID: 5},
+		UpdateData: unsafe.Pointer(targetUpdate),
+	}
+	deps := defaultFlagPickupCTFNativeDeps4EA490()
+	deps.flagIndex = func(got *Object) uint32 {
+		if got != source {
+			t.Fatal("wrong flag-index object")
+		}
+		return 0x102
+	}
+	deps.moveHome = func(got *Object, gotUpdate *FlagUpdateData4EA490) {
+		if got != source || gotUpdate != update {
+			t.Fatal("move did not receive typed cached update")
+		}
+		got.PosVec = gotUpdate.Home
+		target.TeamVal.ID = 9
+	}
+	deps.informReturn = func(netCode uint32) {
+		if netCode != 0x12345678 {
+			t.Fatalf("return net code = %#x", netCode)
+		}
+	}
+	deps.flagStatus = func(teamID, status, index uint8, carrier uint16) int32 {
+		if teamID != 9 || status != 0 || index != 2 || carrier != 0 {
+			t.Fatalf("status = %d/%d/%d/%#x", teamID, status, index, carrier)
+		}
+		return -1
+	}
+	flagPickupCTFNative4EA490(source, target, nil, deps)
+	if source.PosVec != update.Home || update.State != 0 {
+		t.Fatalf("source/state = %v/%d", source.PosVec, update.State)
+	}
+}
+
+func TestFlagPickupCTFNative4EA490ScoresThroughNamedFields(t *testing.T) {
+	sourceUpdate := &FlagUpdateData4EA490{Home: types.Pointf{X: 10, Y: 20}}
+	itemUpdate := &FlagUpdateData4EA490{Home: types.Pointf{X: 30, Y: 40}, State: 7}
+	player := &Player{Lessons: 41}
+	targetUpdate := &PlayerUpdateData{Player: player}
+	item := &Object{
+		ObjClass:   object.ClassFlag,
+		TeamVal:    ObjectTeam{ID: 4},
+		UpdateData: unsafe.Pointer(itemUpdate),
+	}
+	source := &Object{
+		TeamVal:    ObjectTeam{ID: 2},
+		PosVec:     sourceUpdate.Home,
+		UpdateData: unsafe.Pointer(sourceUpdate),
+	}
+	target := &Object{
+		ObjClass:     object.ClassPlayer,
+		NetCode:      0xabcdef01,
+		TeamVal:      ObjectTeam{ID: 2},
+		InvFirstItem: item,
+		UpdateData:   unsafe.Pointer(targetUpdate),
+	}
+	team := &Team{Lessons: 5}
+	deps := defaultFlagPickupCTFNativeDeps4EA490()
+	deps.flagIndex = func(obj *Object) uint32 {
+		if obj == item {
+			return 0x123
+		}
+		return 7
+	}
+	deps.teamByID = func(id uint8) *Team {
+		if id != 2 {
+			t.Fatalf("team id = %d", id)
+		}
+		return team
+	}
+	deps.changeTeamScore = func(got *Team, score int32) {
+		if got != team || score != 6 {
+			t.Fatalf("team score = %p/%d", got, score)
+		}
+	}
+	deps.detachInventory = func(owner, got *Object) {
+		if owner != target || got != item {
+			t.Fatal("wrong detach objects")
+		}
+		itemUpdate.Home = types.Pointf{X: 50, Y: 60}
+	}
+	deps.createAt = func(got, owner *Object, pos types.Pointf) {
+		if got != item || owner != nil || pos != itemUpdate.Home {
+			t.Fatalf("create = %p/%p/%v", got, owner, pos)
+		}
+	}
+	deps.flagStatus = func(teamID, status, index uint8, carrier uint16) int32 {
+		if teamID != 4 || status != 0 || index != 0x23 || carrier != 0 {
+			t.Fatalf("status = %d/%d/%#x/%#x", teamID, status, index, carrier)
+		}
+		return 0
+	}
+	deps.informFlag = func(code, netCode, index uint32) {
+		if code != 5 || netCode != 0xabcdef01 || index != 0x123 {
+			t.Fatalf("inform = %d/%#x/%#x", code, netCode, index)
+		}
+	}
+	flagPickupCTFNative4EA490(source, target, nil, deps)
+	if player.Lessons != 42 {
+		t.Fatalf("player lessons = %d", player.Lessons)
+	}
+	if itemUpdate.State != 0 {
+		t.Fatalf("item state = %d", itemUpdate.State)
+	}
+}
+
+func TestFlagPickupCTFNative4EA490EnemyKeepsCachedPointers(t *testing.T) {
+	sourceUpdate := &FlagUpdateData4EA490{State: 7}
+	replacementSourceUpdate := &FlagUpdateData4EA490{State: 8}
+	oldPlayer := &Player{WeaponEquip: 0x20}
+	newPlayer := &Player{WeaponEquip: 0x40}
+	targetUpdate := &PlayerUpdateData{Player: oldPlayer}
+	replacementTargetUpdate := &PlayerUpdateData{Player: newPlayer}
+	carriedFlag := &Object{ObjClass: object.ClassFlag}
+	nonFlag := &Object{InvNextItem: carriedFlag}
+	source := &Object{
+		TeamVal:    ObjectTeam{ID: 1},
+		UpdateData: unsafe.Pointer(sourceUpdate),
+	}
+	target := &Object{
+		NetCode:      0x12345678,
+		TeamVal:      ObjectTeam{ID: 2},
+		InvFirstItem: nonFlag,
+		UpdateData:   unsafe.Pointer(targetUpdate),
+	}
+	team := &Team{}
+	indexLoads := 0
+	deps := defaultFlagPickupCTFNativeDeps4EA490()
+	deps.flagIndex = func(got *Object) uint32 {
+		indexLoads++
+		if got != source {
+			t.Fatal("wrong index object")
+		}
+		if indexLoads == 1 {
+			return 0x123
+		}
+		return 0x456
+	}
+	deps.teamByID = func(id uint8) *Team {
+		if id != 1 {
+			t.Fatalf("team id = %d", id)
+		}
+		return team
+	}
+	deps.teamEligible = func(got *Team) int32 {
+		if got != team {
+			t.Fatal("wrong eligible team")
+		}
+		return 1
+	}
+	deps.forceDrop = func(owner, item *Object) {
+		if owner != target || item != carriedFlag {
+			t.Fatal("wrong force-drop objects")
+		}
+	}
+	deps.finalizeDelete = func(got *Object) {
+		if got != source {
+			t.Fatal("wrong finalized object")
+		}
+		source.UpdateData = unsafe.Pointer(replacementSourceUpdate)
+	}
+	deps.inventoryPut = func(owner, item *Object, mode int32) {
+		if owner != target || item != source || mode != 1 {
+			t.Fatal("wrong inventory-put args")
+		}
+		target.UpdateData = unsafe.Pointer(replacementTargetUpdate)
+	}
+	deps.reportObject = func(recipient uint32, got *Object) {
+		if recipient != 255 || got != source {
+			t.Fatal("wrong report-object args")
+		}
+	}
+	deps.informFlag = func(code, netCode, index uint32) {
+		if code != 6 || netCode != 0x12345678 || index != 0x456 {
+			t.Fatalf("inform = %d/%#x/%#x", code, netCode, index)
+		}
+		target.NetCode = 0x89abcdef
+	}
+	deps.flagStatus = func(teamID, status, index uint8, carrier uint16) int32 {
+		if teamID != 1 || status != 1 || index != 0x23 || carrier != 0xcdef {
+			t.Fatalf("status = %d/%d/%#x/%#x", teamID, status, index, carrier)
+		}
+		return 0
+	}
+	purged := false
+	deps.purgeBuffs = func(got *Object) {
+		if got != target {
+			t.Fatal("wrong purge target")
+		}
+		purged = true
+	}
+	flagPickupCTFNative4EA490(source, target, nil, deps)
+	if oldPlayer.WeaponEquip != 0x21 || newPlayer.WeaponEquip != 0x40 {
+		t.Fatalf("old/new weapon flags = %#x/%#x", oldPlayer.WeaponEquip, newPlayer.WeaponEquip)
+	}
+	if sourceUpdate.State != 0 || replacementSourceUpdate.State != 8 {
+		t.Fatalf("old/new source states = %d/%d", sourceUpdate.State, replacementSourceUpdate.State)
+	}
+	if !purged || indexLoads != 2 {
+		t.Fatalf("purged/index loads = %t/%d", purged, indexLoads)
+	}
+}

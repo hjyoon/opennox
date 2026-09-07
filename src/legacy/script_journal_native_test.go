afa6faa744b06f8f21522af79637db19b04a71fa
Hyojeon Yoon <hjyoon@kakao.com>
2026-09-08 01:42:53 +0900

    legacy: guard native journal dispatch

---
+++ src/legacy/script_journal_native_test.go
@@ -0,0 +1,18 @@
+package legacy
+
+import (
+	"testing"
+
+	"github.com/opennox/noxscript/ns/asm"
+)
+
+func TestJournalMutationBuiltinsHaveNoLegacyCFallback(t *testing.T) {
+	for _, builtin := range []asm.Builtin{
+		asm.BuiltinJournalDelete,
+		asm.BuiltinJournalEdit,
+	} {
+		if _, ok := CallScriptBuiltin(builtin); ok {
+			t.Fatalf("builtin %d still has a legacy C fallback", builtin)
+		}
+	}
+}
